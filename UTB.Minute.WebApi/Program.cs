12f6e8e58cfd464f766e7d0c50c5ecfaff6f042e
Tomáš Rusnok <t_rusnok@utb.cz>
2026-03-20 12:00:53 +0100

    Remade WebApi (so it doesn't use the template from class) - Still need to complete the rest

--- UTB.Minute.WebApi/Program.cs
+++ UTB.Minute.WebApi/Program.cs
@@ -13,11 +13,8 @@ var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
+app.MapGet("/minuteMeals", WebApiVersion1.GetAllMinuteMeals);
 app.MapPost("/minuteMeals", WebApiVersion1.CreateMinuteMeal);
-app.MapGet("/minuteMeals", WebApiVersion1.GetMinuteMeals);
-app.MapGet("/minuteMeals/{id:int}", WebApiVersion1.GetMinuteMealById);
-app.MapPut("/minuteMeals/{id:int}", WebApiVersion1.UpdateMinuteMeal);
-app.MapDelete("/minuteMeals/{id:int}", WebApiVersion1.DeleteMinuteMeal);
 
 app.UseHttpsRedirection();
 
@@ -25,93 +22,21 @@ app.Run();
 
 public static class WebApiVersion1
 {
-    public static async Task<Created<MinuteMealDto>> CreateMinuteMeal(MinuteMealDto minuteMealDto, MinuteContext context)
+    public static async Task<Ok<MinuteMealDto[]>> GetAllMinuteMeals(MinuteContext context)
     {
-        // Add new meal to db
+        var meals = await context.MinuteMeals.Select(m => new MinuteMealDto(m.Id, m.Desc, m.Price)).ToArrayAsync();
 
-        MinuteMeal m = new() { Desc = minuteMealDto.Desc , Price = minuteMealDto.Price};
-        context.MinuteMeals.Add(m);
+        return TypedResults.Ok(meals);
+    }
+    
+    public static async Task<Created<MinuteMealDto>> CreateMinuteMeal(MinuteContext context)
+    {
+        var meal = new MinuteMeal { Desc = };
+
+        context.Add(meal);
         await context.SaveChangesAsync();
 
-        MinuteMealDto mDto = new MinuteMealDto(m.Id, m.Desc, m.Price);
-
-        return TypedResults.Created($"/minuteMeals/{mDto.MinuteMealId}", mDto);
-    }
-
-    public static async Task<Ok<MinuteMealDto[]>> GetMinuteMeals(MinuteContext context)
-    {
-        // Return all meals from db
-        //2.1 Načíst autory z databáze
-        //2.2 Převést je na typ AuthorDto
-
-        MinuteMeal[] mealArr = await context.MinuteMeals.ToArrayAsync();
-
-        //AuthorDto[] authors = new AuthorDto[poleAutoru.Length];
-
-        MinuteMealDto[] minuteMeals = await context.MinuteMeals.Where(m => m.Desc.ToLower().Contains("Smazak"))
-                                                   .OrderBy(m => m.Desc)
-                                                   .Select(m => new MinuteMealDto(m.Id, m.Desc, m.Price))
-                                                   .ToArrayAsync();
-
-
-        for (int i = 0; i < mealArr.Length; i++)
-        {
-            minuteMeals[i] = new MinuteMealDto(mealArr[i].Id, mealArr[i].Desc, mealArr[i].Price);
-        }
-
-
-        return TypedResults.Ok(minuteMeals);
-    }
-
-    public static async Task<Results<NotFound, Ok<MinuteMealDto>>> GetMinuteMealById(int id,  MinuteContext context)
-    {
-        // Return meal by id
-
-        if (await context.MinuteMeals.FindAsync(id) is MinuteMeal minuteMeal)
-        {
-            MinuteMealDto minuteMealDto = new(minuteMeal.Id, minuteMeal.Desc, minuteMeal.Price);
-
-            return TypedResults.Ok(minuteMealDto);
-        }
-        else
-        {
-            return TypedResults.NotFound();
-        }
-    }
-
-    public static async Task<Results<NoContent, NotFound>> UpdateMinuteMeal(int id, MinuteMealDto minuteMealDto, MinuteContext context)
-    {
-        if (await context.MinuteMeals.FindAsync(id) is MinuteMeal minuteMeal)
-        {
-            // Change meal in db
-
-            minuteMeal.Desc = minuteMealDto.Desc;
-
-            await context.SaveChangesAsync();
-
-            return TypedResults.NoContent();
-        }
-        else
-        {
-            return TypedResults.NotFound();
-        }
-    }
-
-    public static async Task<Results<NoContent, NotFound>> DeleteMinuteMeal(int id, MinuteContext context)
-    {
-        if (await context.MinuteMeals.FindAsync(id) is MinuteMeal minuteMeal)
-        {
-            // Delete meal from db
-
-            context.MinuteMeals.Remove(minuteMeal);
-
-            await context.SaveChangesAsync();
-
-            return TypedResults.NoContent();
-        }
-        else
-        {
-            return TypedResults.NotFound();
-        }
+        MinuteMealDto mealDto = new MinuteMealDto(meal.Id, meal.Desc, meal.Price);
+        return TypedResults.Created($"/minuteMeals/{meal.Id}", mealDto);
     }
 }
