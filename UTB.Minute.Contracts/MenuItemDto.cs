4a41cee17e8c2a3191b6818f35be767259a193d9
Tomáš Rusnok <t_rusnok@utb.cz>
2026-03-31 12:51:26 +0200

    added id for foreign keys

--- UTB.Minute.Contracts/MenuItemDto.cs
+++ UTB.Minute.Contracts/MenuItemDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace UTB.Minute.Contracts
 {
-    public record MenuItemDto(int MenuItemId, DateTime Date, int Portions);
+    public record MenuItemDto(int MenuItemId, DateTime Date, int Portions, int MinuteMealId);
     public record MenuItemRequestDto(DateTime Date, int Portions);
     public record MenuItemPatchDateDto(DateTime Date);
     public record MenuItemPatchPortionsDto(int Portions);
