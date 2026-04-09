cb289973319949f3360c5f0c4eb718316128cdd0
Tomáš Rusnok <t_rusnok@utb.cz>
2026-04-09 11:05:53 +0200

    Revert "Might rollback"
    This reverts commit 51caac2cec42bed6dc78198034a986d6271da7c3.

--- UTB.Minute.AppHost/AppHost.cs
+++ UTB.Minute.AppHost/AppHost.cs
@@ -1,21 +1,9 @@
-using Microsoft.Extensions.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-IResourceBuilder<SqlServerServerResource> sql;
-
-if (builder.Environment.IsEnvironment("Testing"))
-{
-    sql = builder.AddSqlServer("sql-testing", port: 55550)
-                 .WithContainerName("testing-minutky-sql-server");
-}
-else
-{
-    sql = builder.AddSqlServer("sql", port: 55555)
+var sql = builder.AddSqlServer("sql", port:55555)
                  .WithDataVolume()
                  .WithLifetime(ContainerLifetime.Persistent)
                  .WithContainerName("minutky-sql-server");
-}
 
 var database = sql.AddDatabase("database");
 
