55dd81923e72b9bd996ffb27867f8299688e7aaf
Tomáš Rusnok <t_rusnok@utb.cz>
2026-05-12 16:09:23 +0200

    Added keycloak code - Needs to be tested if it works

--- UTB.Minute.WebApi.Tests/TestFixture.cs
+++ UTB.Minute.WebApi.Tests/TestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using Aspire.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Json;
 using UTB.Minute.Db;
 
 namespace UTB.Minute.WebApi.Tests
@@ -7,6 +8,8 @@ namespace UTB.Minute.WebApi.Tests
     public class TestFixture : IAsyncLifetime
     {
         private DistributedApplication app = null!;
+        private HttpClient? keycloakClient;
+        private string? idToken;
         private string? connectionString;
         public HttpClient HttpClient { get; private set; } = null!;
 
@@ -18,12 +21,38 @@ namespace UTB.Minute.WebApi.Tests
             app = await builder.BuildAsync(TestContext.Current.CancellationToken);
 
             await app.StartAsync(TestContext.Current.CancellationToken);
+            
+            await app.ResourceNotifications.WaitForResourceHealthyAsync("keycloak", TestContext.Current.CancellationToken);
+
+            keycloakClient = app.CreateHttpClient("keycloak", "https");
+
+            var response = await keycloakClient.PostAsync("/realms/utb-minute/protocol/openid-connect/token", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "grant_type", "password" },
+                { "client_id", "utb-minute-tests" },
+                { "username", "karel" },
+                { "password", "karel" },
+                { "scope", "openid" }
+            }));
+
+            response.EnsureSuccessStatusCode();
+
+            TokenResponse tokenResponse = await response.Content.ReadFromJsonAsync<TokenResponse>() ?? throw new Xunit.Sdk.XunitException("Token endpoint returned null TokenResponse.");
+
+            if (string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
+            {
+                throw new Xunit.Sdk.XunitException("TokenResponse does not contain AccessToken.");
+            }
+
+            idToken = tokenResponse.IdToken;
 
             await app.ResourceNotifications.WaitForResourceHealthyAsync("database", TestContext.Current.CancellationToken);
             await app.ResourceNotifications.WaitForResourceHealthyAsync("webapi", TestContext.Current.CancellationToken);
 
             connectionString = await app.GetConnectionStringAsync("database", TestContext.Current.CancellationToken);
             HttpClient = app.CreateHttpClient("webapi", "https");
+
+            HttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
 
             using var context = CreateContext();
 
@@ -60,6 +89,20 @@ namespace UTB.Minute.WebApi.Tests
 
         public async ValueTask DisposeAsync()
         {
+            if (keycloakClient is not null)
+            {
+                if (idToken is not null)
+                {
+                    _ = await keycloakClient.PostAsync("/realms/utb-minute/protocol/openid-connect/logout",
+                        new FormUrlEncodedContent(new Dictionary<string, string>
+                        {
+                        { "id_token_hint", idToken }
+                        }));
+                }
+
+                keycloakClient.Dispose();
+            }
+
             HttpClient.Dispose();
             await app.DisposeAsync();
             GC.SuppressFinalize(this);
